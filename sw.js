6262c13876596bd1e9df776d712ff0e853261554
likaiqiang <likaiqiang.frontend@gmail.com>
2022-02-02 02:03:11 +0000

    Deploying to gh-pages from @ likaiqiang/learn@db2100112b4596063cccb0cd52f9687c0e1885c8 🚀

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"c223375c1f3dda40c67fc76274f94da3","url":"./index.html"},{"revision":"67ef93b7026979d99da673cfd9fd85de","url":"./js/main.js"},{"revision":"225ab40e391dde3ac6bb57a36242e07a","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"f19a28224d39630adc44ec6c90571f92","url":"./index.html"},{"revision":"67ef93b7026979d99da673cfd9fd85de","url":"./js/main.js"},{"revision":"225ab40e391dde3ac6bb57a36242e07a","url":"./css/index.css"}],{
     directoryIndex: null
 });
 
